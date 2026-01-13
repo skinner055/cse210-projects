397a300ae5e8746c2e066f5c92ebcffdd75b8814
skinner055 <ski25006@byui.edu>
2026-01-13 14:45:46 -0700

    Finished Assignments

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,37 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        string letter = null;
+        Console.WriteLine("What is your grade percentage? ");
+        int grade = Convert.ToInt32(Console.ReadLine());
+        if (grade >= 90)
+            {
+                letter = "A";
+            }
+        else if (grade < 90 && grade >= 80)
+            {
+                letter = "B";
+            }
+        else if (grade < 80 && grade >= 70) 
+            {
+                letter = "C";
+            }
+        else if (grade < 70 && grade >= 60) 
+            {
+                letter = "D";
+            } 
+        else
+            {
+                letter = "F";
+            }
+        Console.WriteLine($"Your grade is {letter}.");
+        if (grade >= 70)
+            {
+                Console.WriteLine("Congratulations! You Passed!");
+            }
+        else
+            {
+                Console.WriteLine("Better Luck Next Time.");
+            }
     }
 }
